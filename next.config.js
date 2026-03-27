ffc42fc0b6314bd2f9595d1ff0dec8d1bc37f9bd
yummyyyy123 <125256333+yummyyyy123@users.noreply.github.com>
2026-03-27 13:00:52 +0800

    Add transpilePackages to fix module resolution issues

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     config.experiments = { ...config.experiments, asyncWebAssembly: true }
     return config
-  }
+  },
+  transpilePackages: ['rust-security-service']
 }
 
 module.exports = nextConfig
